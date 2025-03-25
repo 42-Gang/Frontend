0eb3ebedefe84aa081e8ddf23b3028d49bdc7411
yeni023 <yeeun3011@naver.com>
2025-03-25 09:56:11 +0900

    feat: 2강 승자 프로필 및 VS 텍스트 UI 추가

--- Frontend/src/pages/Tournament/Matching.tsx
+++ Frontend/src/pages/Tournament/Matching.tsx
@@ -13,13 +13,21 @@ import {
   LineWrapper,
   VerticalLine,
   HorizontalLine,
+  WinnerGrid,
+  VsText,
 } from "./Matching";
 
+// 임시 유저 정보
 const mockUsers = [
   { id: 1, name: "PONG", profileImage: BasicProfile1 },
   { id: 2, name: "DING", profileImage: BasicProfile2 },
   { id: 3, name: "PING", profileImage: BasicProfile1 },
   { id: 4, name: "DONG", profileImage: BasicProfile2 },
+];
+
+const mockWinners = [
+  { id: 1, name: "PING", profileImage: BasicProfile1 },
+  { id: 2, name: "DING", profileImage: BasicProfile2 },
 ];
 
 const Matching = () => {
@@ -33,12 +41,22 @@ const Matching = () => {
     <Wrapper>
       <ProfileOverlay>
         <LineWrapper>
-          {/* 왼쪽 세로선 + 가로선 */}
+          {/* 중앙 상단 승자 이미지 */}
+          <WinnerGrid>
+            {mockWinners.map((user) => (
+              <UserProfile key={user.id} isReady={isReady}>
+                <UserImage src={user.profileImage} alt={user.name} />
+                <UserName>{user.name}</UserName>
+              </UserProfile>
+            ))}
+          </WinnerGrid>
+          <VsText>VS</VsText>
+          {/* 대진표 구분선 - 왼쪽 */}
           <VerticalLine left="46px" />
           <HorizontalLine left="46px" />
-          {/* 오른쪽 세로선 + 가로선 */}
+          {/* 대진표 구분선 - 오른쪽 */}
           <VerticalLine left="420px" />
-          <HorizontalLine left="335px" />{" "}
+          <HorizontalLine left="335px" />
           <UserGrid>
             {mockUsers.map((user) => (
               <UserProfile key={user.id} isReady={isReady}>
