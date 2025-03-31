370583fa45413f5acf22cb43afb9dc013ffe9598
yeni023 <yeeun3011@naver.com>
2025-03-31 16:38:44 +0900

    feat: 상대방 Ready 상태 반영 및 Toast 표시

--- Frontend/src/pages/Tournament/Matching.ts
+++ Frontend/src/pages/Tournament/Matching.ts
@@ -1,5 +1,6 @@
-import styled, { css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 import { LINE_HEIGHT, LINE_COLOR } from "./LineConstants";
+import { ToastContainer } from "react-toastify";
 
 export const Wrapper = styled.div`
   position: relative;
@@ -100,4 +101,34 @@ export const ExitImage = styled.img`
   width: 200px;
   height: auto;
   object-fit: contain;
+`;
+
+export const CustomToastContainer = styled(ToastContainer)`
+  margin-left: 50px;
+`;
+
+export const ToastStyle = createGlobalStyle`
+  .custom-toast {
+    width: 180px !important;
+    min-width: unset !important;
+    max-width: 180px !important;
+    padding: 6px 10px !important;
+    font-size: 13px !important;
+    border-radius: 5px !important;
+    background-color: white !important;
+    color: black !important;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+
+    .Toastify__toast-icon {
+      width: 16px !important;
+      height: 16px !important;
+      margin-right: 6px !important;
+    }
+
+    .Toastify__progress-bar {
+      height: 3px !important;
+    }
+  }
 `;
